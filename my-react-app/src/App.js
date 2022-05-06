e8ddc4975cbb3376e00f5175eee3e5fcec5a61bb
Manojb999 <manojbaviskar963@gmail.com>
2022-05-06 17:35:15 +0530

    basic code

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      people : []
+    };
+  }
+
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=> response.json())
+    .then((users) =>this.setState(
+      () => {
+        return {people : users}
+      },
+      () => {
+        console.log(this.state);
+      }
+    )
+    );
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.people.map(
+            (p) => {
+              return (
+                  <div key={p.id}>
+                    <h1>Hi {p.name}</h1>
+                  </div>
+                );
+              }
+            )
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
